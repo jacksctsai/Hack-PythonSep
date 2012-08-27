d80f16c56bcb097870e57ccfddf4998ed04ebf04
CCC <ccc.larc@gmail.com>
2012-08-27 23:50:34 +0800

    [core] extract piece property

--- tetris-tkinter.py
+++ tetris-tkinter.py
@@ -9,6 +9,29 @@ BOARD_WIDTH = 10
 BOARD_HEIGHT = 20
 
 #===============================================================================
+# piece
+#===============================================================================
+I_PIECE = 0xf
+J_PIECE = 0x2e # J
+L_PIECE = 0x47 # L
+O_PIECE = 0x66
+S_PIECE = 0xC6 # S
+T_PIECE = 0x27
+Z_PIECE = 0x6C # Z
+
+ALL_PIECES = [I_PIECE, J_PIECE, L_PIECE, O_PIECE, S_PIECE, T_PIECE, Z_PIECE]
+
+PIECE_COLOR = {
+    I_PIECE: "red",
+    J_PIECE: "#0f0",
+    L_PIECE: "#ff0",
+    O_PIECE: "#0ff",
+    S_PIECE: "#38f",
+    T_PIECE: "blue",
+    Z_PIECE: "#f0f"
+}
+
+#===============================================================================
 # drawing transform
 #===============================================================================
 H, W = 30, 30
@@ -25,12 +48,14 @@ def ui_create_rect(i, j, color):
     scr.create_rectangle(x0, y0, x1, y1, fill=color)
 
 
+#===============================================================================
+# 
+#===============================================================================
 score = 0
-blk = {0xf:"red", 0x2e:"#0f0", 0x27:"blue", 0x47:"#ff0", 0x66:"#0ff", 0xC6:"#38f", 0x6C:"#f0f"}
 board = [ [0xf if j == BOARD_HEIGHT else 0] * BOARD_WIDTH + [0xf] * 2 for j in range(BOARD_HEIGHT + 2 + 1) ]
 new_piece = lambda pc: ([((z >> 2) + 1, z & 3) for z in range(16) if (pc >> z) & 1], 3, -2, pc)
 collide = lambda piece, px, py: [1 for (i, j) in piece if board[j + py][i + px]] #是否碰撞
-piece, px, py, pc = new_piece(random.choice(blk.keys())) # 第一個piece
+piece, px, py, pc = new_piece(random.choice(ALL_PIECES)) # 第一個piece
 
 def tick(e=None):
     global piece, px, py, pc, tickcnt, board, score
@@ -46,7 +71,7 @@ def tick(e=None):
         if collide(piece, px, py + 1):
             if py < 0: sys.exit("GAME OVER: score %i" % score) # game over 的狀況
             for i, j in piece: board[j + py][i + px] = pc
-            piece, px, py, pc = new_piece(random.choice(blk.keys()))
+            piece, px, py, pc = new_piece(random.choice(ALL_PIECES))
         else: py += 1
 
         nb = [l for l in board[:BOARD_HEIGHT] if 0 in l] + board[BOARD_HEIGHT:] # 沒有被填滿的
@@ -57,9 +82,9 @@ def tick(e=None):
 
     for i, j in [(i, j) for i in range(BOARD_WIDTH) for j in range(BOARD_HEIGHT)]:
         if (i - px, j - py) in piece:
-            color = blk[pc]
+            color = PIECE_COLOR[pc]
         else:
-            color = blk.get(board[j][i], "#000")
+            color = PIECE_COLOR.get(board[j][i], "#000")
         ui_create_rect(i, j, color)
 
 
