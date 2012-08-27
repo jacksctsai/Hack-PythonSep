73e1281fce09e8763a1b780ddbb7aa11582e0627
CCC <ccc.larc@gmail.com>
2012-08-28 00:14:59 +0800

    [core] extract PIECE_SHAPE

--- tetris-tkinter.py
+++ tetris-tkinter.py
@@ -1,4 +1,5 @@
 ﻿# -*- coding: utf-8 -*-
+import copy
 import random
 import sys
 import Tkinter
@@ -9,18 +10,29 @@ import Tkinter
 BOARD_WIDTH = 10
 BOARD_HEIGHT = 20
 
+PIECE_INIT_X = 3
+PIECE_INIT_Y = -2
+
 #===============================================================================
 # piece
 #===============================================================================
 I_PIECE = 0xf
-J_PIECE = 0x2e # J
-L_PIECE = 0x47 # L
+J_PIECE = 0x2e
+L_PIECE = 0x47
 O_PIECE = 0x66
-S_PIECE = 0xC6 # S
+S_PIECE = 0xC6
 T_PIECE = 0x27
-Z_PIECE = 0x6C # Z
+Z_PIECE = 0x6C
 
-ALL_PIECES = [I_PIECE, J_PIECE, L_PIECE, O_PIECE, S_PIECE, T_PIECE, Z_PIECE]
+ALL_PIECES = [
+    I_PIECE,
+    J_PIECE,
+    L_PIECE,
+    O_PIECE,
+    S_PIECE,
+    T_PIECE,
+    Z_PIECE
+]
 
 PIECE_COLOR = {
     I_PIECE: "red",
@@ -31,6 +43,24 @@ PIECE_COLOR = {
     T_PIECE: "blue",
     Z_PIECE: "#f0f"
 }
+
+"""
+shape = lambda pc: [((z >> 2) + 1, z & 3) for z in range(16) if (pc >> z) & 1]
+"""
+PIECE_SHAPE = {
+    I_PIECE: [(1, 0), (1, 1), (1, 2), (1, 3)],
+    J_PIECE: [(1, 1), (1, 2), (1, 3), (2, 1)],
+    L_PIECE: [(1, 0), (1, 1), (1, 2), (2, 2)],
+    O_PIECE: [(1, 1), (1, 2), (2, 1), (2, 2)],
+    S_PIECE: [(1, 1), (1, 2), (2, 2), (2, 3)],
+    T_PIECE: [(1, 0), (1, 1), (1, 2), (2, 1)],
+    Z_PIECE: [(1, 2), (1, 3), (2, 1), (2, 2)]
+}
+
+def new_piece():
+    p = random.choice(ALL_PIECES)
+    p_shape = copy.deepcopy(PIECE_SHAPE[p])
+    return p_shape, p
 
 #===============================================================================
 # drawing transform
@@ -65,9 +95,14 @@ def redraw_ui():
 #===============================================================================
 score = 0
 board = [ [0xf if j == BOARD_HEIGHT else 0] * BOARD_WIDTH + [0xf] * 2 for j in range(BOARD_HEIGHT + 2 + 1) ]
-new_piece = lambda pc: ([((z >> 2) + 1, z & 3) for z in range(16) if (pc >> z) & 1], 3, -2, pc)
 collide = lambda piece, px, py: [1 for (i, j) in piece if board[j + py][i + px]] #是否碰撞
-piece, px, py, pc = new_piece(random.choice(ALL_PIECES)) # 第一個piece
+
+
+#===============================================================================
+# 
+#===============================================================================
+piece, pc = new_piece() # 第一個piece
+px, py = PIECE_INIT_X, PIECE_INIT_Y
 
 def tick(e=None):
     global piece, px, py, pc, tickcnt, board, score
@@ -83,7 +118,8 @@ def tick(e=None):
         if collide(piece, px, py + 1):
             if py < 0: sys.exit("GAME OVER: score %i" % score) # game over 的狀況
             for i, j in piece: board[j + py][i + px] = pc
-            piece, px, py, pc = new_piece(random.choice(ALL_PIECES))
+            piece, pc = new_piece()
+            px, py = PIECE_INIT_X, PIECE_INIT_Y
         else: py += 1
 
         nb = [l for l in board[:BOARD_HEIGHT] if 0 in l] + board[BOARD_HEIGHT:] # 沒有被填滿的
