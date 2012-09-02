add5838e99d5ca1b10245432026147d6f07275cc
CCC <ccc.larc@gmail.com>
2012-09-02 18:05:13 +0800

    [core] use import visual

--- tetris-visual.py
+++ tetris-visual.py
@@ -13,7 +13,7 @@ from __future__ import division
 from random import choice
 import sound
 import time
-from visual import *
+import visual
 any = __builtins__.any
 
 
@@ -24,23 +24,23 @@ BOARD_WIDTH = 10
 BOARD_HEIGHT = 20
 
 score, R, N, T = 0, 0.9, 100, 0.5
-scene.center = ((BOARD_WIDTH - 1) / 2, (BOARD_HEIGHT - 1) / 2)
-scene.width = BOARD_WIDTH * 35
-scene.height = BOARD_HEIGHT * 35
-scene.forward = (0, 0, +1)
-scene.up = 0, -1, 0
-scene.lights = \
-  [distant_light(direction=(0.22, 0.44, -0.88), color=color.gray(0.8)),
-   distant_light(direction=(-0.88, -0.22, +0.44), color=color.gray(0.3))]
-scene.autoscale = False
-#scene.show_rendertime = 1
-scene.pause = False
+visual.scene.center = ((BOARD_WIDTH - 1) / 2, (BOARD_HEIGHT - 1) / 2)
+visual.scene.width = BOARD_WIDTH * 35
+visual.scene.height = BOARD_HEIGHT * 35
+visual.scene.forward = (0, 0, +1)
+visual.scene.up = 0, -1, 0
+visual.scene.lights = [
+    visual.distant_light(direction=(0.22, 0.44, -0.88), color=visual.color.gray(0.8)),
+    visual.distant_light(direction=(-0.88, -0.22, +0.44), color=visual.color.gray(0.3))]
+visual.scene.autoscale = False
+#visual.scene.show_rendertime = 1
+visual.scene.pause = False
 
 blk = { 0x0f:(0, 0, 1), 0x2e:(0, 1, 1), 0x27:(0, 1, 0), 0x47:(1, 0, 1), 0xC6:(1, 0, 0), 0x6C:(1, 1, 0), 0x66:(1, 0.6, 0) }
 
 new_piece = lambda pc: ([((z >> 2) + 1, z & 3) for z in xrange(16) if (pc >> z) & 1], 3, -2, pc)
-new_focus = lambda piece, pc: [box(pos=p, color=blk[pc], size=(R, R, R)) for p in piece]
-points(pos=[(x, y) for x in xrange(BOARD_WIDTH) for y in xrange(BOARD_HEIGHT)])
+new_focus = lambda piece, pc: [visual.box(pos=p, color=blk[pc], size=(R, R, R)) for p in piece]
+visual.points(pos=[(x, y) for x in xrange(BOARD_WIDTH) for y in xrange(BOARD_HEIGHT)])
 
 
 #===============================================================================
@@ -128,9 +128,9 @@ def clear_complete_lines():
 
     # 消去
     sound.distroy_sound.play()
-    d_line = [obj for obj in scene.objects if type(obj) is box and obj.y in fn]
+    d_line = [obj for obj in visual.scene.objects if type(obj) is visual.box and obj.y in fn]
     for _ in xrange(10):
-        rate(20)
+        visual.rate(20)
         for obj in d_line:
             obj.opacity -= 1 / 10
     for obj in d_line:
@@ -138,7 +138,7 @@ def clear_complete_lines():
 
     # 下降
     for n in fn:
-        for obj in (obj for obj in scene.objects if type(obj) is box and obj.y < n):
+        for obj in (obj for obj in visual.scene.objects if type(obj) is visual.box and obj.y < n):
             obj.y += 1
 
     return fn
@@ -163,7 +163,7 @@ def tick(t_stamp=[time.time(), 0]):
 
   # 自動處理
   t_stamp[1] = time.time()
-  if t_stamp[1] - t_stamp[0] > T and not scene.pause:
+  if t_stamp[1] - t_stamp[0] > T and not visual.scene.pause:
     if not collide(piece, px, py + 1): #自動落下
       py += 1
 
@@ -185,8 +185,8 @@ def tick(t_stamp=[time.time(), 0]):
     t_stamp[0] = t_stamp[1]
 
   # 鍵盤控制
-  if scene.kb.keys:
-    key = scene.kb.getkey()
+  if visual.scene.kb.keys:
+    key = visual.scene.kb.getkey()
     if key in ('down', 'j'):
       py = (j for j in xrange(py, BOARD_HEIGHT) if collide(piece, px, j + 1)).next()# 找出第一個會碰撞的
     elif key in ('up', 'k'):
@@ -197,18 +197,19 @@ def tick(t_stamp=[time.time(), 0]):
       npx = px + (-1 if key in ('left', 'h', 'H') else 1)
       if not collide(piece, npx, py): px = npx
     elif key == 'p':
-      scene.pause = switch_pause()
+      visual.scene.pause = switch_pause()
 
   # 方塊位置變更
-  for i in xrange(4): focus[i].pos = vector(px, py) + piece[i]
+  for i in xrange(4): focus[i].pos = visual.vector(px, py) + piece[i]
+
 
 # mainloop
 piece, px, py, pc = new_piece(choice(blk.keys()))
 focus = new_focus(piece, pc)
 sound.init_sound()
 while 1:
-  rate(N)
-  tick()
+    visual.rate(N)
+    tick()
 
 
 
